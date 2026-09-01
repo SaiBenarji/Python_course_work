65b4df2b039571b8d10e12b11dcd3eef172cdc0f
Dasari Sai Benarji <dasarisai896@gmail.com>
2026-09-01 22:39:36 +0530

    For Loops & Nested if

---
+++ Day_12/forloop.py
@@ -0,0 +1,50 @@
+'''a='Sai Benarji'
+for ch in a:
+    if ch in 'aeiouAEIOU':
+         print(ch)'''
+
+'''b=[1,2,3,4,5,6,7,8,9,10,11,22,30,45]
+for b in b:
+    if b % 2 == 0:
+        print(b ,'Even')
+    else:
+        print(b, 'Odd')'''
+
+
+'''marks=(20,35,50,75,90)
+for marks in marks:
+    if marks >35:
+        print(marks, 'Pass')
+    else:
+        print(marks, 'Fail')'''
+
+
+'''f={'sai','benarji','kowshik','python'}
+for i in f:
+    print(i)'''
+
+'''bus={'s1':'Booked','s2':'Available','s3':'Booked','s4':'Available','s5':'Booked'}
+for seat in bus:
+    if bus.get(seat)=='Available':
+        print(seat, bus.get(seat))'''
+# Range (Start, end+1, step)
+'''
+for i in range(1, 11):
+    print(i)
+
+for i in range(2,51,2):
+    print(i)  
+
+for i in range(1,100,2):
+    print(i)
+
+for i in range(5,51,5):
+    print(i)  
+
+n=int(input('enter the number:'))
+for i in range(1,21):
+    print(f'{n} * {i} = {n*i}')
+
+'''
+
+
---
+++ Day_12/nestedif.py
@@ -0,0 +1,34 @@
+'''a=eval(input('Follows Account: '))
+b=eval(input('Close Friends: '))
+
+if a:
+    if b:
+        print('story Visible')
+    else:
+        print('Not in Close friends List')
+else:
+    print('Follow the Account First')'''
+
+
+'''a=eval(input('Check the Registration:'))
+b=eval(input('check the Payment:'))
+
+if a:
+    if b:
+        print('Tournament Entry Confirmed')
+    else:
+        print('Entry Fee Pending')
+else:
+    print('Registration Required First')'''
+
+
+a=eval(input('Check the Link Active:'))
+b=eval(input('Check the Permission:'))
+
+if a:
+    if b:
+        print('File Open Successfully')
+    else:
+        print('Access Denined')    
+else:
+    print('Invalid File Link')    
