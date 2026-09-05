17b07f2d5e19fd5bfe3d35863a8ec6ddc9d8cd23
Dasari Sai Benarji <dasarisai896@gmail.com>
2026-09-05 14:35:47 +0530

    ATM & Cal

---
+++ Day_24/cal/logic.py
@@ -0,0 +1,24 @@
+def add(a,b):
+    print('Add:',a+b)
+    
+    
+def sub(a,b):
+    print('Sub:',a-b)
+    
+    
+def mul(a,b):
+    print('Mul:',a*b)
+    
+    
+def div(a,b):
+    print('Div:',a/b)
+    
+    
+def rem(a,b):
+    print('Rem:',a**b)
+    
+    
+def exp(a,b):
+    print('Exp:',a%b)
+    
+    
---
+++ Day_24/cal/main.py
@@ -0,0 +1,30 @@
+'''
+import logic
+logic.add(5,6)
+logic.sub(5,9)
+logic.mul(5,4)
+logic.div(5,20)
+logic.rem(5,9)
+logic.exp(2,3)
+
+import logic as lg
+lg.add(5,6)
+lg.sub(5,9)
+lg.mul(5,4)
+lg.div(5,20)
+lg.rem(5,9)
+lg.exp(2,3)
+
+
+from logic import add,div
+add(2,3)
+div(4,2)
+'''
+
+from logic import *
+add(5,6)
+sub(5,9)
+mul(5,4)
+div(5,20)
+rem(5,9)
+exp(2,3)
